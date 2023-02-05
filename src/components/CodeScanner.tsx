2e9a28171fad8ef6da49ed87f6a29eda3108d2c8
Mohd Haider <haiderahmed12786@gmail.com>
2023-02-05 11:09:09 +0530

    barcode scanner done

--- src/components/CodeScanner.tsx
+++ src/components/CodeScanner.tsx
@@ -1,33 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-export const CodeScanner = () => {
-	const [data, setData] = useState<string>("Not Found");
-	const [stopStream, setStopStream] = useState<boolean>(true);
+function CodeScanner() {
+	const [data, setData] = useState<string[]>([]);
+	const [torchOn, setTorchOn] = useState(false);
+	const [show, setShow] = useState(false);
+
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
 	return (
 		<>
-			<button
-				className="flex justify-center items-center"
-				onClick={() => setStopStream(false)}
-			>
-				Scan
-			</button>
+			<div className=" flex flex-col">
+				{show && (
+					<BarcodeScannerComponent
+						width={500}
+						height={500}
+						delay={1000}
+						torch={torchOn}
+						onUpdate={(err: any, result: any) => {
+							if (result) {
+								const newData = [...data, result.text];
+								setData(newData);
+								alert(result.text);
+								console.log(data);
+							} else {
+								console.log("No found");
+							}
+						}}
+					/>
+				)}
 
-			<BarcodeScannerComponent
-				width={500}
-				height={500}
-				stopStream={stopStream}
-				onUpdate={(err, result) => {
-					if (result) {
-						console.log(result);
-						setStopStream(true);
-					} else {
-						console.log(err);
-						setData("Not Found");
-					}
-				}}
-			/>
-			<p>{data}</p>
+				{data?.length > 0 &&
+					data?.map((item) => (
+						<p className="text-gray-500 " key={item}>
+							{item}
+						</p>
+					))}
+				{!(data.length > 0) && "No data found"}
+
+				<button
+					className="  bg-red-400 "
+					onClick={() => setTorchOn(!torchOn)}
+				>
+					Switch Torch {torchOn ? "Off" : "On"}
+				</button>
+				<button className="bg-blue-400" onClick={() => setShow(!show)}>
+					Switch Carema {show ? "Off" : "On"}
+				</button>
+			</div>
 		</>
 	);
-};
+}
+
+export default CodeScanner;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 import Barcode from "react-barcode";
-import { CodeScanner } from "../components/CodeScanner";
+import CodeScanner from "../components/CodeScanner";
 
 // const socket = io("http://localhost:5000");
 
@@ -9,11 +9,11 @@ export const Home = () => {
 	return (
 		<div>
 			<div className="flex justify-center items-center">
-				<Barcode value="123456789012" displayValue={false} />
+				<Barcode value="63d8" displayValue={true} />
 			</div>
 			<CodeScanner />
 
-			<h1>Home</h1>
+			{/* <h1>Home</h1> */}
 		</div>
 	);
 };
