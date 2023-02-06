a153f3231608c934bd93047c175b2de710036347
Mohd Haider <haiderahmed12786@gmail.com>
2023-02-06 20:22:17 +0530

    rrd installed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			<Home />
-			{/* <Products /> */}
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/products" element={<Products />} />
+
+					{/* <Home /> */}
+					{/* <Products /> */}
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
