0aa60cf5cdf62123ffc3f348087e55d391101373
Mohd Haider <haiderahmed12786@gmail.com>
2023-02-06 20:02:08 +0530

    some changes in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 // import io from "socket.io-client";
-import CodeScanner from "../components/CodeScanner";
 import axios from "axios";
 //redux
 import { useSelector, useDispatch } from "react-redux";
 import { selectProduct, addProduct } from "../redux/slice/productSlice";
 import type { RootState } from "../redux/store";
+// components
+import CodeScanner from "../components/CodeScanner";
 // const socket = io("http://localhost:5000");
-// import { compressor } from "../functions/functions";
 
 const Home = () => {
 	const products = useSelector((state: RootState) => state.product.items);
