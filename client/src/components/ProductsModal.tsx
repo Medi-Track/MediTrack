e530387d55d369cbc0332ae34500096ec348113c
Mohd Haider <haiderahmed12786@gmail.com>
2023-02-11 21:55:07 +0530

    page created

--- client/src/components/ProductsModal.tsx
+++ client/src/components/ProductsModal.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { Link } from "react-router-dom";
 // components
 import Item from "./molecules/Item";
 // redux
@@ -72,17 +73,19 @@ const ProductsModal = ({
 									>
 										<div className="flex justify-between items-center">
 											<span>Scanned Product</span>
-											<button
-												type="button"
-												className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-												onClick={() => {
-													if (!data) return;
-													dispatch(addScannedProduct(data));
-													closeModal();
-												}}
-											>
-												Done
-											</button>
+											<Link to="/scanned-medicine">
+												<button
+													type="button"
+													className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+													onClick={() => {
+														if (!data) return;
+														dispatch(addScannedProduct(data));
+														closeModal();
+													}}
+												>
+													Done
+												</button>
+											</Link>
 										</div>
 									</Dialog.Title>
 									{data && <Item item={data} />}
--- client/src/pages/ScannedMedicine.tsx
+++ client/src/pages/ScannedMedicine.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+//redux
+import { useSelector, useDispatch } from "react-redux";
+import Item from "../components/molecules/Item";
+import { RootState } from "../redux/store";
+
+//types
+import { Product } from "../types";
 
 const ScannedMedicine = () => {
+	const medicines: Product[] = useSelector(
+		(state: RootState) => state.scannedProduct.medicine
+	);
 	return (
 		<div>
-			<h1>Scanned Medicine</h1>
+			{medicines?.length > 0 &&
+				medicines?.map((medicine) => (
+					<Item key={medicine._id} item={medicine} />
+				))}
 		</div>
 	);
 };
