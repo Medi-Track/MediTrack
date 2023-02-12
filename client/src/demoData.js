c82b1da43a37db245487ae01e0641888987f5e07
MAXASSASSINS <mohammedshadab23april@gmail.com>
2023-02-12 19:18:01 +0530

    demo data for app

---
+++ client/src/demoData.js
@@ -0,0 +1,114 @@
+export const demoData = [
+    {
+        "title": "Dr Morepen Gluco One BG 03 Blood Glucose Test Strip (Only Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "Tata 1mg Milk Thistle Capsules for Liver Detox, 70% Silymarin with Dandelion & Ginger is a health supplement enriched with antioxidants that can be beneficial for alcoholic and non-alcoholic fatty liver conditions. Milk thistle may help protect the liver from the harmful effects of toxins and reduce inflammation. The capsules contain dandelion, that aid in reducing the level of excess fat stored in the liver, and ginger helps enhance bioavailability"
+    },
+    {
+        "title": "Tata 1mg Milk Thistle Veg Capsule for Liver Detox, 70% Silymarin with Dandelion & Ginger, Supports Liver Detox & Prevents Fatty Liver",
+        "price": 6337,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 18,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule",
+        "price": 31,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 99,
+        "dec": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule contains highly potent ingredients such as betacarotene, zinc sulphate and selenium. It is useful in supporting prostate health, strengthening the immune system and promoting skin health. Moreover, the formulation is scientifically formulated, gluten-free and non-GMO."
+    },
+    {
+        "title": "Dr Morepent Strip (Only Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "Dr. Morepen Muscle Food BCCA Curious Cola",
+        "price": 67,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 85,
+        "dec": "Dr. Morepen Muscle Food BCCA Berry Burst is a health supplement beneficial for individuals who work out regularly. It can help speed up the nutrient absorption process directly into the muscles, not the liver. It can help promote lean muscles and recovery from workout sessions. It is a vegan-friendly supplement with no added sugar in it."
+    },
+    {
+        "title": "Dr Morepen Gluco One BG 03 Blood Glucose Test Strily Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "Tata 1mg Milk Thistle Capsules for Liver Detox, 70% Silymarin with Dandelion & Ginger is a health supplement enriched with antioxidants that can be beneficial for alcoholic and non-alcoholic fatty liver conditions. Milk thistle may help protect the liver from the harmful effects of toxins and reduce inflammation. The capsules contain dandelion, that aid in reducing the level of excess fat stored in the liver, and ginger helps enhance bioavailability"
+    },
+    {
+        "title": "Tata 1mg Milk Thistle Veg Capsule for Liver Detox, 70% Silymarin with Dandelion & Ginger, Supports Liver Detox & Prevents Fatty Liver",
+        "price": 6337,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 18,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule",
+        "price": 31,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 99,
+        "dec": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule contains highly potent ingredients such as betacarotene, zinc sulphate and selenium. It is useful in supporting prostate health, strengthening the immune system and promoting skin health. Moreover, the formulation is scientifically formulated, gluten-free and non-GMO."
+    },
+    {
+        "title": "Dr Morepen Gluco One Bip (Only Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "Dr. Morepen Muscle Food BCCA Curious Cola",
+        "price": 67,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 85,
+        "dec": "Dr. Morepen Muscle Food BCCA Berry Burst is a health supplement beneficial for individuals who work out regularly. It can help speed up the nutrient absorption process directly into the muscles, not the liver. It can help promote lean muscles and recovery from workout sessions. It is a vegan-friendly supplement with no added sugar in it."
+    },
+    {
+        "title": "Gluco One BG 03 Blood Glucose Test Strip (Only Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "Tata 1mg Milk Thistle Capsules for Liver Detox, 70% Silymarin with Dandelion & Ginger is a health supplement enriched with antioxidants that can be beneficial for alcoholic and non-alcoholic fatty liver conditions. Milk thistle may help protect the liver from the harmful effects of toxins and reduce inflammation. The capsules contain dandelion, that aid in reducing the level of excess fat stored in the liver, and ginger helps enhance bioavailability"
+    },
+    {
+        "title": "Tata 1mg Milk Thistle Veg Capsule for Liver Detox, 70% Silymarin with Dandelion & Ginger, Supports Liver Detox & Prevents Fatty Liver",
+        "price": 6337,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 18,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule",
+        "price": 31,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 99,
+        "dec": "1mg Super Antioxidant Multiminerals with Betacarotene Capsule contains highly potent ingredients such as betacarotene, zinc sulphate and selenium. It is useful in supporting prostate health, strengthening the immune system and promoting skin health. Moreover, the formulation is scientifically formulated, gluten-free and non-GMO."
+    },
+    {
+        "title": "Dr Strip (Only Strips)",
+        "price": 637,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 8,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable and convenient self-testing method with a blood glucose monitor. It helps to provide quick and reliable results in the comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    },
+    {
+        "title": "Dr. Morepen Muscle Food BCCA Curious Cola",
+        "price": 67,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 85,
+        "dec": "Dr. Morepen Muscle Food BCCA Berry Burst is a health supplement beneficial for individuals who work out regularly. It can help speed up the nutrient absorption process directly into the muscles, not the liver. It can help promote lean muscles and recovery from workout sessions. It is a vegan-friendly supplement with no added sugar in it."
+    },
+    {
+        "title": "Activist Powder Protein Kesar",
+        "price": 21,
+        "brand": "Morepen Laboratories Ltd",
+        "stock": 20,
+        "dec": "These test strips help to measure the concentration of glucose in the blood. It is a reliable anhe comfort of your place. It can be used for both professional (at clinics) and personal (at home) use. It is effortless to use."
+    }
+]
