064f7035a4b41a5659aea83ea88f025e4de58fe3
Mohd Haider <haiderahmed12786@gmail.com>
2023-02-12 21:57:16 +0530

    some bug fixed

--- client/src/components/molecules/Item.tsx
+++ client/src/components/molecules/Item.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 //redux
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -8,8 +8,15 @@ import {
 //types
 import { Product } from "../../types";
 
-const Item = ({ item, singleItem }: any) => {
+const Item = ({ item, alreadyScanned, stock, setStock }: any) => {
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		if (alreadyScanned) {
+			setStock(item?.stock);
+		}
+	}, [alreadyScanned]);
+
 	return (
 		<li
 			key={item?._id}
@@ -19,15 +26,22 @@ const Item = ({ item, singleItem }: any) => {
 			<div className="text-left flex items-center">
 				<span
 					onClick={() => {
-						item?.stock > 0 && dispatch(decreaseQuantity(item));
+						item?.stock > 0 &&
+							dispatch(decreaseQuantity(item)) &&
+							alreadyScanned &&
+							setStock(stock - 1);
 					}}
 					className=" p-2 rounded-sm h-fit border-cyan-200 hover:bg-cyan-200 cursor-pointer text-2xl"
 				>
 					-
 				</span>
-				<span> {item?.stock}</span>
+				<span> {alreadyScanned ? stock : item?.stock}</span>
 				<span
-					onClick={() => dispatch(increaseQuantity(item))}
+					onClick={() =>
+						dispatch(increaseQuantity(item)) &&
+						alreadyScanned &&
+						setStock(stock + 1)
+					}
 					className="p-2 rounded-sm h-fit border-cyan-200 hover:bg-cyan-200 cursor-pointer text-2xl"
 				>
 					+
@@ -35,7 +49,7 @@ const Item = ({ item, singleItem }: any) => {
 			</div>
 			<div className="text-right">₹{item?.price}</div>
 			<button className="rounded m-l-6 p-1 cursor-default bg-cyan-400 hover:text-cyan-400">
-				₹{item?.stock * item?.price}
+				₹{(alreadyScanned ? stock : item?.stock) * item?.price}
 			</button>
 		</li>
 
